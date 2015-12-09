4fec8af6817723b9fcea04bb51ea74973d344502
Alex Bander <alexbander.ibb@gmail.com>
2015-12-09 13:45:45 +0100

    Fehlerkorrektur und Methode 4 und 5

--- zinsen/Program.cs
+++ zinsen/Program.cs
@@ -4,7 +4,7 @@
 #                                                                      #
 #                      for- und while-Schleifen                        #
 #                                am                                    #
-#                  Beispiel einer Zins Berrechnung                     #
+#                  Beispiel einer Zins Berechnung                     #
 #                                                                      #
 #                           C# - Projekt                               #
 #                                                                      #
@@ -32,22 +32,25 @@ namespace zinsen
             {
                 Console.WriteLine(Frage);
                 string tmp = Console.ReadLine();
-                int input;
-                if (int.TryParse(tmp, out input))
+                Int16 input;
+                if (Int16.TryParse(tmp, out input))
                 {
                         int result = Convert.ToInt16(input);
                         return result;
                     }
                 else
                     {
-                        Console.WriteLine("Fehlerhafte Eingabe, ENTER zum wiederholen");
+                        Console.WriteLine("Fehlerhafte Eingabe (integer), ENTER zum wiederholen");
                         Console.ReadKey();
                         Ask.i(Frage);
                       return 0;
                     }
-                }
-            internal static double d(string Frage)
+            }
+
+            internal static double d(string Bitt_Frage_Eintragen)
             {
+
+                Console.WriteLine(Bitt_Frage_Eintragen);
                 string tmp = Console.ReadLine();
                 double input;
                 if (double.TryParse(tmp, out input))
@@ -57,9 +60,9 @@ namespace zinsen
                 }
                 else
                 {
-                    Console.WriteLine("Fehlerhafte Eingabe, ENTER zum wiederholen");
+                    Console.WriteLine("Fehlerhafte Eingabe (double), ENTER zum wiederholen");
                     Console.ReadKey();
-                    Ask.i(Frage);
+                    Ask.i(Bitt_Frage_Eintragen);
                     return 0;
                 }
             }
@@ -75,17 +78,19 @@ namespace zinsen
     static void Main()
         {
             Console.OutputEncoding = Encoding.UTF8;
-            Console.WriteLine("### Zins Programm (Einfach) ### \n Wähle Programm: \n 1. - Watzlaw \n 2. - Alex2: for Header Bedingungen \n 3. - Alex3: for Zähler(i) only \n 4. Alex4: while Kopfgeführt \n 5. Alex5: while Fußgeführt \n 6. ### Ende ###");
+            Console.WriteLine("### Zins Programm (Einfach) ### \n Wähle Programm: \n 1. - Watzlaw \n 2. - Alex2: for Header Bedingungen \n 3. - Alex3: for Zähler(i) only \n 4. Alex4: while Kopf-geführt \n 5. Alex5: while Fuß-geführt \n 6. ### Ende ###");
 
             int c = Convert.ToInt16(Console.ReadLine());
             if (c == 1) { watzlaw(); }
             else if (c == 2) { Alex2(); }
             else if (c == 3) { Alex3(); }
+            else if (c == 4) { Alex4(); }
+            else if (c == 5) { Alex5(); }
             else { return; }
 
         }
 
-        static void watzlaw()
+    static void watzlaw()
         {
 
             double var1;                        // Betrag
@@ -133,15 +138,15 @@ namespace zinsen
             }
 
             Console.ReadKey();
+            Console.Clear();
             Main();
         }
 
     static void Alex2()
         {
-
+            Console.OutputEncoding = Encoding.UTF8;
             int startjahr = Ask.i("Wann ist das Anfangsjahr? ");
             int endjahr = Ask.i("Welches ist das Endjahr? ");
-
             double startkapital = Ask.d("Wie hoch ist das Startkapital? ");
             double prozentsatz = Ask.d("Wie hoch ist der Zinssatz");
             int j = endjahr - startjahr;
@@ -156,9 +161,10 @@ namespace zinsen
                 Console.WriteLine("im Jahr: " + startjahr + " ein Wert von {0:c} \n"+" Endjahr: "+endjahr, k);
             }
             Console.ReadKey();
+            Console.Clear();
             Main();
         }
-        static void Alex3()
+    static void Alex3()
         {
 
             Console.WriteLine("Startjahr: ");
@@ -179,11 +185,65 @@ namespace zinsen
                 int jahr = startjahr + i;
                 k = startkapital + (startkapital * (prozentsatz / 100));
                 startkapital = k;
-//                                                                     fkapital = fkapital + (fkapital * (prozentsatz / 100));
+//                                                                     fkapital = fkapital + (fkapital * (Prozentsatz / 100));
                 Console.WriteLine("im Jahr: " + jahr + " ein Wert von {0:c} \n", k);
-//                                                                     Console.WriteLine("im Jahr: " + jahr + " ein Wert von {0:c} \n", fkapital);
+//                                                                     Console.WriteLine("im Jahr: " + Jahr + " ein Wert von {0:c} \n", fkapital);
             }
             Console.ReadKey();
+            Console.Clear();
+            Main();
+        }
+    static void Alex4()
+        {
+
+            Console.WriteLine("Startjahr: ");
+            int startjahr = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Startkapital: ");
+            double startkapital = Convert.ToDouble(Console.ReadLine()); ;
+            Console.WriteLine("Prozentsatz: ");
+            double prozentsatz = Convert.ToDouble(Console.ReadLine()); ;
+            Console.WriteLine("Startjahr: ");
+            int endjahr = Convert.ToInt16(Console.ReadLine());
+            int j = endjahr - startjahr;
+            double k;
+            int i=0;
+
+            while (i < j)
+            //                                                                     for (fkapital = 24, i = 0; i <= j; i++)
+            {
+                int jahr = startjahr + i;
+                k = startkapital + (startkapital * (prozentsatz / 100));
+                startkapital = k;
+                //                                                                     fkapital = fkapital + (fkapital * (Prozentsatz / 100));
+                Console.WriteLine("im Jahr: " + jahr + " ein Wert von {0:c} \n", k);
+                //                                                                     Console.WriteLine("im Jahr: " + Jahr + " ein Wert von {0:c} \n", fkapital);
+            }
+            Console.ReadKey();
+            Console.Clear();
+            Main();
+        }
+    static void Alex5()
+        {
+            int startjahr = Ask.i("Wann ist das Anfangsjahr? ");
+            int endjahr = Ask.i("Welches ist das Endjahr? ");
+            double startkapital = Ask.d("Wie hoch ist das Startkapital? ");
+            double prozentsatz = Ask.d("Wie hoch ist der Zinssatz");
+            int j = endjahr - startjahr;
+            double k;
+            int x = 0;
+            int jahr;
+
+            do
+            {
+                jahr = startjahr + x;
+                k = startkapital + (startkapital * (prozentsatz / 100));
+                startkapital = k;
+                Console.WriteLine("im Jahr: " + jahr + " ein Wert von {0:c} \n", k);
+                Console.ReadKey();
+            }
+            while (x < j);
+            Console.ReadKey();
+            Console.Clear();
             Main();
         }
 
